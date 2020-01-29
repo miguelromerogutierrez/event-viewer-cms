41c50901e7c29bc30b980df2c0e33328573df4a6
Miguel Romero <miguel.angel.romero.gtz@gmail.com>
2020-01-29 11:47:30 -0600

    Add avatares and converter markdown to html

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -1,6 +1,7 @@
 'use strict';
 const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-
+let showdown = require('showdown');
+let converter = new showdown.Converter();
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -23,5 +24,26 @@ module.exports = {
       );
       return sanitizeEntity(entity, { model: strapi.models.event });
     }
-  }
+  },
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.event.search(ctx.query);
+    } else {
+      entities = await strapi.services.event.find(ctx.query);
+    }
+    if(entities && entities.length > 0) {
+      entities = entities.map(entity => {
+        if(entity.description) {
+          entity.description = converter.makeHtml(entity.description);
+        }
+        return entity;
+      });
+    }
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.event })
+    );
+  },
 };
